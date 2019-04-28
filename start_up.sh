d8030c84513621dcac9bd9065a6a2e84263324f6
Xiao Mi <homilly@users.noreply.github.com>
2019-04-28 20:37:23 +0800

    Update start_up.sh

--- start_up.sh
+++ start_up.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#
+# This script allows you to launch several images
+# from this repository once they're built.
+#
+# Make sure you add the `docker run` command
+# in the header of the Dockerfile so the script
+# can find it and execute it.
+
 
